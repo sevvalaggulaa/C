306c197bd4f1752526b398f3ac04237b79826907
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-03-03 21:13:54 +0300

    Add files via upload

---
+++ 104.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum renkler
+{
+	mavi,
+	kirmizi,
+	yesil,
+	
+}renk;
+
+int main()
+{
+    renk can=kirmizi;
+    printf("%d",can);
+    
+   return 0;
+}
